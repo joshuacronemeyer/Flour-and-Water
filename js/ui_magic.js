bf164c5724fd401b2a0daa3efb19d9cb7468be89
joshuacronemeyer <jrc@thoughtworks.com>
2011-02-20 01:18:15 -0800

    deploying latest changes to gh pages

--- js/ui_magic.js
+++ js/ui_magic.js
@@ -1,3 +1,22 @@
+// Hydration = x is a web based calculator for sourdough bakers.
+// Copyright (C) 2011  Josh Cronemeyer
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// For more information please visit the project on github: 
+// https://github.com/joshuacronemeyer/Flour-and-Water
+
 function setStarterHydration(){
   var percentWidth = parseInt(($('#hydration').width() / $('#starter').width())*100)
   $('#hydration').find('.quality').text(percentWidth);
@@ -13,8 +32,9 @@ function setTweetText(){
 }
 
 function updateHydration() {
-   $(this).find('.quantity').text($(this).width());
-    calculator.resetHydration();
+  $(this).find('.quantity').text($(this).width());
+  calculator.resetHydration();
+  $("#info-result").text($("#result").text());
 }
 
 function updateStarterAndHydration() { 
@@ -32,8 +52,14 @@ function resizeableParams(updateCallback){
   }
 }
 $(function() {
-  $( "#flour" ).resizable(resizeableParams(updateHydration));
-  $( "#water" ).resizable(resizeableParams(updateHydration));
-  $( "#starter" ).resizable($.extend(resizeableParams(updateStarterAndHydration), {alsoResize: "#hydration"}));
-	$( "#hydration" ).resizable($.extend(resizeableParams(setStarterHydration), {containment: "#starter"}));
+  $("#info").click(function(){$("#overlay").show();});
+  $("#overlay").click(function(){
+    if ($("#overlay").not(":visible")) {
+      $("#overlay").hide();
+    }
+  });
+  $("#flour").resizable(resizeableParams(updateHydration));
+  $("#water").resizable(resizeableParams(updateHydration));
+  $("#starter").resizable($.extend(resizeableParams(updateStarterAndHydration), {alsoResize: "#hydration"}));
+	$("#hydration").resizable($.extend(resizeableParams(setStarterHydration), {containment: "#starter"}));
 });

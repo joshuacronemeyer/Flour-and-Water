d8f9d21b8486776caa41c35faf735bb8e09b5e9b
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2011-08-12 22:37:42 -0700

    playing with touch events

--- public/js/events.js
+++ public/js/events.js
@@ -17,7 +17,40 @@
 // For more information please visit the project on github: 
 // https://github.com/joshuacronemeyer/Flour-and-Water
 
+function touchHandler(event)
+{
+ var touches = event.changedTouches,
+    first = touches[0],
+    type = "";
+
+     switch(event.type)
+{
+    case "touchstart": type = "mousedown"; break;
+    case "touchmove":  type="mousemove"; break;        
+    case "touchend":   type="mouseup"; break;
+    default: return;
+}
+var simulatedEvent = document.createEvent("MouseEvent");
+simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                          first.screenX, first.screenY,
+                          first.clientX, first.clientY, false,
+                          false, false, false, 0/*left*/, null);
+
+first.target.dispatchEvent(simulatedEvent);
+event.preventDefault();
+}
+
+function init()
+{
+   document.addEventListener("touchstart", touchHandler, true);
+   document.addEventListener("touchmove", touchHandler, true);
+   document.addEventListener("touchend", touchHandler, true);
+   document.addEventListener("touchcancel", touchHandler, true);    
+}
+
+
 $(function() {
+  init();
   resetCalculator();
   setTweetText();
   $("#lock").click(toggleLock);
